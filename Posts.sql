ddc8c733966c6b5576ca66aa08f0fb380333b400
Forreste <Forrestesmith@gmail.com>
2023-12-15 11:48:27 -0500

    Add views, reply, like, loves, user(firstname),(familyname), date, time, userimage to Posts table

--- Posts.sql
+++ Posts.sql
@@ -2,7 +2,7 @@
 This section creates the Posts table in the Ancient_Civ_Database. This needs to be
 connected to likes, commentsm users and sperated by families(which are seperated by Civs)
 
-
+views, reply, like, loves, user(firstname),(familyname), date, time, userimage
 
  */
 
---
+++ UserNotification.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[UserNotification]
+(
+  [Id] INT NOT NULL PRIMARY KEY
+)
